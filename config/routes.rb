df442801e2915891a9223b7f1aa833a91bb78648
david lee <davidslee10@gmail.com>
2015-09-10 16:39:27 +0000

    created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GymCityMembers::Application.routes.draw do
+
+  root'homes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
