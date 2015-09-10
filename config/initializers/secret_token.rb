bf84322f091a1fb0fa1b9001c353778ee9b6e295
david lee <davidslee10@gmail.com>
2015-09-10 16:26:00 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GymCityMembers::Application.config.secret_key_base = '5d76def80b45da7629610cb61a1feceb717a91527d26fbdaa3d3fe2723550e41ce2bc0994f9ea0a7d68b6f498a95e2782f6156dff37f433e72d5f2e645c2ba26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GymCityMembers::Application.config.session_store :cookie_store, key: '_gym-city-members_session'
